0de140b67aa92208f046f3074f94760834b147b8
Nullsans <eneskorkmaz572@gmail.com>
2026-08-03 12:14:52 +0300

    Create 7.c

---
+++ odev1/7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Int: %zu\n", sizeof(int));
+
+    printf("Float: %zu\n", sizeof(float));
+
+    printf("Double: %zu\n", sizeof(double));
+
+    printf("Char: %zu\n", sizeof(char));
+
+    return 0;
+}
