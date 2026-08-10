15bd487971251e821062e85c84b1f56f0f16d5eb
Nullsans <eneskorkmaz572@gmail.com>
2026-08-10 20:17:23 +0300

    Create 2.c

---
+++ odev3/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+   char c = 'a'; 
+
+ 
+
+   // Gıcıklığıma tek satırda yazdım :D
+
+   if (c == 'a' || c == 'e' || c == 'ı' || c == 'i' || c == 'o' || c == 'ö' || c == 'u' || c == 'ü') { printf("Bu %c Karakteri Bir Ünlü.Beyaz Olur Kendisi\n", c); } 
+
+   else { 
+
+      printf("Sıradan bir mahlukat\n"); 
+
+   }
+
+   
+
+   return 0;
+
+}
+
+
