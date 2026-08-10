6635a8c00f682474e15ba6cc431d3a870bf020cd
Nullsans <eneskorkmaz572@gmail.com>
2026-08-10 20:03:37 +0300

    Add even/odd check to main function in 1.c

---
+++ odev3/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int sayi = 4; //sayıyı tanımla 
+    
+    if (sayi % 2 == 0) { printf("Bu sayı çift: %d", sayi); } //kontrol et
+    
+    else { printf("Bu sayı tek: %d", sayi); } // değilse değildir yani çok uzatma :D
+    
+    return 0;
+}
