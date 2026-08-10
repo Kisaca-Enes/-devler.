c83c08739ce54a8cacd38c55be06328451b86581
Nullsans <eneskorkmaz572@gmail.com>
2026-08-10 20:31:52 +0300

    Create 4.c

---
+++ odev3/4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    
+    if (a > 0) { printf("Pozitif"); }
+    
+    else if (a < 0) { printf("Negatif"); }
+                
+    else { printf("Nötür"); }
+            
+             
+}
